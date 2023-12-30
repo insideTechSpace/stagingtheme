ecd8f9a2e642cc7f298f52e3dcda43efe929e06a
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2023-12-30 17:12:45 -0400

    change for default topic list

--- javascripts/discourse/api-initializers/its-template.js
+++ javascripts/discourse/api-initializers/its-template.js
@@ -27,13 +27,13 @@ export default {
             "discovery.new",
             "discovery.top",
             "tag.show",
-           // "category.show",
+            //"category.show",
           ];
           return allowedRoutes.includes(currentRoute);
         },
       });
 
-      api.onPageChange((url, title) => {
+      api.onPageChange((url, title) => { 
         const itsHidden = localStorage.getItem("its_panel_hidden") === "true";
         const itsVisible = localStorage.getItem("its_panel_hidden") === "false";
         const isHidden = document.body.classList.contains("its-panel-hidden");
@@ -42,6 +42,32 @@ export default {
           document.body.classList.add("its-panel-hidden");
         } else if (itsVisible && isHidden) {
           document.body.classList.remove("its-panel-hidden");
+        }
+
+        // Apply custom topic-list-item template only when the current route matches one of the allowed routes
+        if (this._shouldUseCustomTemplate()) {
+          api.modifyClass("component:topic-list-item", {
+            pluginId: "its-template",
+
+            renderTopicListItem() {
+              const template = findRawTemplate("list/custom-topic-list-item");
+              if (template) {
+                this.set("topicListItemContents", htmlSafe(template(this)));
+              }
+            },
+          });
+        } else {
+          // Otherwise use the default template-list-item
+          api.modifyClass("component:topic-list-item", {
+            pluginId: "its-template",
+
+            renderTopicListItem() {
+              const template = findRawTemplate("list/default-topic-list-item");
+              if (template) {
+                this.set("topicListItemContents", htmlSafe(template(this)));
+              }
+            },
+          });
         }
       });
     });
