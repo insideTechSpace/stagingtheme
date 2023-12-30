55826ac2320168c0b08be490db8fb6e5af9b2356
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2023-12-30 18:13:53 -0400

    Trial 3

---
+++ javascripts/discourse/api-initializers/custom-topic-list.js
@@ -0,0 +1,30 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { findRawTemplate } from "discourse-common/lib/raw-templates";
+import { htmlSafe } from "@ember/template";
+
+export default {
+  name: "fkbpro",
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      api.modifyClass("component:topic-list-item", {
+        pluginId: "fkb-template",
+
+        renderTopicListItem() {
+          const isLatestPostsPage = api.container.lookup("router:main").currentRouteName === "discovery.latest";
+
+          let templateName = "list/default-topic-list-item";
+
+          if (isLatestPostsPage) {
+            templateName = "list/custom-topic-list-item";
+          }
+
+          const template = findRawTemplate(templateName);
+
+          if (template) {
+            this.set("topicListItemContents", htmlSafe(template(this)));
+          }
+        },
+      });
+    });
+  },
+};
