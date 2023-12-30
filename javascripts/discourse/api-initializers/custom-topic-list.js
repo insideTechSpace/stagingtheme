f99d2ea84f11efeaadbe7d60de288635c5e824cc
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2023-12-30 19:58:28 -0400

    custom tl only on latesst

--- javascripts/discourse/api-initializers/custom-topic-list.js
+++ javascripts/discourse/api-initializers/custom-topic-list.js
@@ -1,6 +1,7 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { findRawTemplate } from "discourse-common/lib/raw-templates";
 import { htmlSafe } from "@ember/template";
+import { addObserver, removeObserver } from "@ember/object/observers";
 
 export default {
   name: "fkbpro",
@@ -9,19 +10,36 @@ export default {
       api.modifyClass("component:topic-list-item", {
         pluginId: "fkb-template",
 
+        addCustomBodyClass(isLatestPostsPage) {
+          const bodyElement = document.querySelector("body");
+
+          if (bodyElement) {
+            if (isLatestPostsPage) {
+              bodyElement.classList.add("custom-tl");
+            } else {
+              bodyElement.classList.remove("custom-tl");
+            }
+          }
+        },
+
+        setupObservers() {
+          const router = api.container.lookup("router:main");
+
+          addObserver(this, "attrs.isLatestPostsPage", this, "addCustomBodyClass");
+
+          this.addCustomBodyClass(router.currentRouteName === "discovery.latest");
+        },
+
+        destroy() {
+          removeObserver(this, "attrs.isLatestPostsPage", this, "addCustomBodyClass");
+        },
+
         renderTopicListItem() {
-          const isLatestPostsPage = api.container.lookup("router:main").currentRouteName === "discovery.latest";
+          const isLatestPostsPage = this.attrs.isLatestPostsPage;
+          this.setupObservers();
 
           let templateName = isLatestPostsPage ? "list/custom-topic-list-item" : "components/topic-list-item";
           let template = findRawTemplate(templateName);
-
-          // Add custom body class to Discourse latest page
-          if (isLatestPostsPage && template) {
-            const bodyElement = document.querySelector('body');
-            if (bodyElement) {
-              bodyElement.classList.add('custom-tl');
-            }
-          }
 
           // Fallback to the original Discourse template if neither custom nor default is available
           if (!template) {
