e28b25eb3697e584559c72fe8c676fa751aa4240
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2024-01-04 04:47:16 -0400

    12

--- javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
+++ javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
@@ -6,10 +6,10 @@ export default {
   name: "ltlitemp",
   initialize() {
     withPluginApi("0.8.7", (api) => {
-      // Override the latest-topic-list-item template
-      api.rawTemplate("components/latest-topic-list-item.hbs", `
-        {{raw-html topicListItemContents}}
-      `, { id: "latest-topic-list-item-override" });
+      // Replace the latest-topic-list-item template
+      api.replaceWith("components/latest-topic-list-item", "list/custom-latest-topic-list-item", {
+        id: "latest-topic-list-item-override"
+      });
 
       // Modify the component to use the custom template
       api.modifyClass("component:latest-topic-list-item", {
