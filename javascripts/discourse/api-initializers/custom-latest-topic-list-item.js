8a106723d72f6a7ad4c229062d1b29ddc648bc76
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2024-01-04 03:01:49 -0400

    2

--- javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
+++ javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
@@ -1,19 +1,15 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { htmlSafe } from "@ember/template";
 import { schedule } from "@ember/runloop";
-import { getOwner } from "@ember/application";
 
 export default {
   name: "ltlitemp",
   initialize() {
     withPluginApi("0.8.7", (api) => {
-      // Fetch the theme ID
-      const themeId = api.container.lookup("site-settings:main").theme_id;
-
       // Override the latest-topic-list-item template
-      api.rawTemplate("connectors/latest-topic-list-item/default.hbs", `
+      api.rawTemplate("components/latest-topic-list-item.hbs", `
         {{raw-html topicListItemContents}}
-      `, { id: `latest-topic-list-item-override-${themeId}` });
+      `, { id: "latest-topic-list-item-override" });
 
       // Modify the component to use the custom template
       api.modifyClass("component:latest-topic-list-item", {
@@ -27,9 +23,7 @@ export default {
               htmlSafe(customTemplate(this))
             );
             schedule("afterRender", () => {
-                if (this.isDestroyed || this.isDestroying) {
-                    return;
-                  }
+              // ... your existing code here
             });
           } else {
             // If the custom template doesn't exist, fall back to the default behavior
