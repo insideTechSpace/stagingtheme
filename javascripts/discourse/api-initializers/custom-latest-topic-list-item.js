d38e6d04fa88c1583a96f317211208ea0f8cb0ad
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2024-01-04 04:45:09 -0400

    11

--- javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
+++ javascripts/discourse/api-initializers/custom-latest-topic-list-item.js
@@ -1,3 +1,36 @@
-import { replaceInTemplate } from 'discourse-common/lib/template';
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { htmlSafe } from "@ember/template";
+import { schedule } from "@ember/runloop";
 
-replaceInTemplate('latest-topic-list-item', 'components/latest-topic-list-item.hbs');
+export default {
+  name: "ltlitemp",
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      // Override the latest-topic-list-item template
+      api.rawTemplate("components/latest-topic-list-item.hbs", `
+        {{raw-html topicListItemContents}}
+      `, { id: "latest-topic-list-item-override" });
+
+      // Modify the component to use the custom template
+      api.modifyClass("component:latest-topic-list-item", {
+        pluginId: "ltli-template",
+
+        renderLatestTopicListItem() {
+          const customTemplate = api.rawTemplates["list/custom-latest-topic-list-item"];
+          if (customTemplate) {
+            this.set(
+              "topicListItemContents", 
+              htmlSafe(customTemplate(this))
+            );
+            schedule("afterRender", () => {
+              // ... your existing code here
+            });
+          } else {
+            // If the custom template doesn't exist, fall back to the default behavior
+            this._super();
+          }
+        },
+      });
+    });
+  },
+};
